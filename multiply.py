bb288b25c24fd3863fbdfda2451070e4c11d3e06
madidiefenbach <madidiefenbach@gmail.com>
2025-02-20 13:33:55 -0700

    created multiply function
    Takes in two numbers and multiplies them

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Madison Diefenbach
+# 02/ 20/25
+# take two numbers and multiply them
+
+def multiply_two_numbers(num1, num2):
+    results = num1 * num2
+    return results
+
+print(multiply_two_numbers(2,3))
